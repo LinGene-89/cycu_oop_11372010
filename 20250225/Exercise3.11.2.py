31b791592942edf15e3efbcab6b911729aea517a
林金元11372010 <gene0931767136@gmail.com>
2025-02-25 20:27:58 +0800

    Create Exercise3.11.2.py

---
+++ 20250225/Exercise3.11.2.py
@@ -0,0 +1,12 @@
+#3.11.2
+str1 = 'Monty'
+str2 = "Python's"
+str3 = 'Flying Circus'
+
+massage1 = ' ' * (40-len(str1)) + str1
+massage2 = ' ' * (40-len(str2)) + str2
+massage3 = ' ' * (40-len(str3)) + str3
+
+print(massage1)
+print(massage2)
+print(massage3)
