91a0eab9ab746470b5f74d6f52c5c507c1a4f9d2
林金元11372010 <gene0931767136@gmail.com>
2025-02-25 20:37:19 +0800

    Update Exercise3.11.2.py

--- 20250225/Exercise3.11.2.py
+++ 20250225/Exercise3.11.2.py
@@ -1,12 +1,7 @@
 #3.11.2
-str1 = 'Monty'
-str2 = "Python's"
-str3 = 'Flying Circus'
+def print_right(s):
+    print(' '*(40-len(s))+s)
 
-massage1 = ' ' * (40-len(str1)) + str1
-massage2 = ' ' * (40-len(str2)) + str2
-massage3 = ' ' * (40-len(str3)) + str3
-
-print(massage1)
-print(massage2)
-print(massage3)
+print_right('Monty')
+print_right("Python's")
+print_right('Flying Circus')
